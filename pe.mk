3b0c31046742f77b04eddbd018d47b8176b305d2
vanya elizarov <elizarov.vanya@gmail.com>
2023-03-28 02:18:51 +0300

    feat: Use Vulkan renderer to resolve issue with "inverse" video

--- pe.mk
+++ pe.mk
@@ -14,4 +14,6 @@ TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
 TARGET_DISABLE_BLUETOOTH_LE_READ_BUFFER_SIZE_V2 := true
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.system.ota.json_url=https://raw.githubusercontent.com/ponces/treble_build_pe/thirteen-plus/ota.json
+    ro.system.ota.json_url=https://raw.githubusercontent.com/ponces/treble_build_pe/thirteen-plus/ota.json \
+    ro.hwui.use_vulkan=true \
+    debug.hwui.renderer=skiavk
